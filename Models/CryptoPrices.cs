0c46b824847a7cdb3420cf2f4cf5325304c2cc60
Devligan <sriharshachallangi@gmail.com>
2026-03-18 20:31:17 -0500

    update

--- Models/CryptoPrices.cs
+++ Models/CryptoPrices.cs
@@ -4,6 +4,10 @@ public class CryptoPrice
 {
     public int Id { get; set; }
     public string Symbol { get; set; } = "";
-    public decimal Price { get; set; }
-    public DateTime Timestamp { get; set; }
+    public decimal Open { get; set; }
+    public decimal High { get; set; }
+    public decimal Low { get; set; }
+    public decimal Close { get; set; }
+    public decimal Volume { get; set; }
+    public DateTime Date { get; set; }
 }
