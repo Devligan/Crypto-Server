4a3e2838db3a3a1c267bac0a1e247c8e397f6880
Devligan <sriharshachallangi@gmail.com>
2026-03-19 18:40:18 -0500

    cleaning

--- DTOs/CyrptoPriceDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CryptoDataPipeline.DTOs;
-
-public class CryptoPriceDto
-{
-    public string Symbol { get; set; } = "";
-    public decimal Close { get; set; }
-    public decimal Volume { get; set; }
-    public DateTime Date { get; set; }
-}
